446094f71d5b4e424ebd219116edfcc3186d563f
Mohcine <mohcine.jaad2023@gmail.com>
2024-05-26 12:00:08 +0100

    implementation of the sidebar

---
+++ assets/admin/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/framework.css">
+    <link rel="stylesheet" href="../css/dashbord.css">
+    <link rel="stylesheet" href="../css/normalize.css" />
+    <link rel="stylesheet" href="../css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet" />
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <div class="page d-flex">
+        <div class="sidebar bg-fff p-20 p-relative">
+            <h3 class="p-relative txt-c mt-0">B.T.S</h3>
+            <ul>
+                <li>
+                    <a href="index.php" class="active d-flex align-c fs-14 color-000 rad-6 p-10">
+                        <i class="fa-solid fa-chart-simple fa-fw"></i>
+                        <span class="fs-14 ml-10">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php" class="d-flex align-c fs-14 color-000 rad-6 p-10">
+                        <i class="fa-solid fa-chalkboard-user fa-fw"></i>
+                        <span class="fs-14 ml-10">Professeur</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php" class="d-flex align-c fs-14 color-000 rad-6 p-10">
+                        <i class="fa-solid fa-user-graduate fa-fw"></i>
+                        <span class="fs-14 ml-10">Étudient</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php" class="d-flex align-c fs-14 color-000 rad-6 p-10">
+                        <i class="fa-solid fa-newspaper fa-fw"></i>
+                        <span class="fs-14 ml-10">Publication</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php" class="d-flex align-c fs-14 color-000 rad-6 p-10">
+                        <i class="fa-solid fa-calendar-xmark fa-fw"></i>
+                        <span class="fs-14 ml-10">Absence</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php" class="d-flex align-c fs-14 color-000 rad-6 p-10">
+                        <i class="fa-solid fa-user-gear fa-fw"></i>
+                        <span class="fs-14 ml-10">Paramètres</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <link rel="stylesheet" href="assets/css/all.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="assets/js/main.js" defer></script>
     <link 
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" 
         rel="stylesheet"
     />
+    <script src="assets/js/main.js" defer></script>
     <title>E-BTS</title>
 </head>
 <body>
